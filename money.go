ed647cedeada88723d580bba70f507d7f06711bf
Tyler Bunnell <tylerbunnell@gmail.com>
2017-04-19 12:22:51 -0600

    Move calculator to new mutator object

--- money.go
+++ money.go
@@ -14,7 +14,6 @@ type Amount struct {
 type Money struct {
 	amount   *Amount
 	currency *Currency
-	calc     *calculator
 }
 
 // New creates and returns new instance of Money
@@ -22,7 +21,6 @@ func New(amount int64, code string) *Money {
 	return &Money{
 		amount:   &Amount{val: amount},
 		currency: newCurrency(code),
-		calc:     &calculator{},
 	}
 }
 
@@ -117,12 +115,12 @@ func (m *Money) IsNegative() bool {
 
 // Absolute returns new Money struct from given Money using absolute monetary value
 func (m *Money) Absolute() *Money {
-	return &Money{amount: m.calc.absolute(m.amount), currency: m.currency, calc: &calculator{}}
+	return &Money{amount: mutate.calc.absolute(m.amount), currency: m.currency}
 }
 
 // Negative returns new Money struct from given Money using negative monetary value
 func (m *Money) Negative() *Money {
-	return &Money{amount: m.calc.negative(m.amount), currency: m.currency, calc: &calculator{}}
+	return &Money{amount: mutate.calc.negative(m.amount), currency: m.currency}
 }
 
 // Add returns new Money struct with value representing sum of Self and Other Money
@@ -131,7 +129,7 @@ func (m *Money) Add(om *Money) (*Money, error) {
 		return nil, err
 	}
 
-	return &Money{amount: m.calc.add(m.amount, om.amount), currency: m.currency, calc: &calculator{}}, nil
+	return &Money{amount: mutate.calc.add(m.amount, om.amount), currency: m.currency}, nil
 }
 
 // Subtract returns new Money struct with value representing difference of Self and Other Money
@@ -140,22 +138,22 @@ func (m *Money) Subtract(om *Money) (*Money, error) {
 		return nil, err
 	}
 
-	return &Money{amount: m.calc.subtract(m.amount, om.amount), currency: m.currency, calc: &calculator{}}, nil
+	return &Money{amount: mutate.calc.subtract(m.amount, om.amount), currency: m.currency}, nil
 }
 
 // Multiply returns new Money struct with value representing Self multiplied value by multiplier
 func (m *Money) Multiply(mul int64) *Money {
-	return &Money{amount: m.calc.multiply(m.amount, mul), currency: m.currency, calc: &calculator{}}
+	return &Money{amount: mutate.calc.multiply(m.amount, mul), currency: m.currency}
 }
 
 // Divide returns new Money struct with value representing Self division value by given divider
 func (m *Money) Divide(div int64) *Money {
-	return &Money{amount: m.calc.divide(m.amount, div), currency: m.currency, calc: &calculator{}}
+	return &Money{amount: mutate.calc.divide(m.amount, div), currency: m.currency}
 }
 
 // Round returns new Money struct with value rounded to nearest zero
 func (m *Money) Round() *Money {
-	return &Money{amount: m.calc.round(m.amount), currency: m.currency, calc: &calculator{}}
+	return &Money{amount: mutate.calc.round(m.amount), currency: m.currency}
 }
 
 // Split returns slice of Money structs with split Self value in given number.
@@ -166,18 +164,18 @@ func (m *Money) Split(n int) ([]*Money, error) {
 		return nil, errors.New("Split must be higher than zero")
 	}
 
-	a := m.calc.divide(m.amount, int64(n))
+	a := mutate.calc.divide(m.amount, int64(n))
 	ms := make([]*Money, n)
 
 	for i := 0; i < n; i++ {
-		ms[i] = &Money{amount: a, currency: m.currency, calc: &calculator{}}
+		ms[i] = &Money{amount: a, currency: m.currency}
 	}
 
-	l := m.calc.modulus(m.amount, int64(n)).val
+	l := mutate.calc.modulus(m.amount, int64(n)).val
 
 	// Add leftovers to the first parties
 	for p := 0; l != 0; p++ {
-		ms[p].amount = m.calc.add(ms[p].amount, &Amount{1})
+		ms[p].amount = mutate.calc.add(ms[p].amount, &Amount{1})
 		l--
 	}
 
@@ -202,9 +200,8 @@ func (m *Money) Allocate(rs []int) ([]*Money, error) {
 	var ms []*Money
 	for _, r := range rs {
 		party := &Money{
-			amount:   m.calc.allocate(m.amount, r, sum),
+			amount:   mutate.calc.allocate(m.amount, r, sum),
 			currency: m.currency,
-			calc:     &calculator{},
 		}
 
 		ms = append(ms, party)
@@ -219,7 +216,7 @@ func (m *Money) Allocate(rs []int) ([]*Money, error) {
 	}
 
 	for p := 0; lo != 0; p++ {
-		ms[p].amount = m.calc.add(ms[p].amount, &Amount{sub})
+		ms[p].amount = mutate.calc.add(ms[p].amount, &Amount{sub})
 		lo -= sub
 	}
 
