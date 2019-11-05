291ac7ff874e331070908a7965614224f2dca913
tegk <t.esseling@protonmail.com>
2019-11-05 10:00:38 +0000

    gofmt -s

--- money.go
+++ money.go
@@ -202,7 +202,7 @@ func (m *Money) Allocate(rs ...int) ([]*Money, error) {
 	}
 
 	var total int64
-  ms := make([]*Money, 0, len(rs))
+	ms := make([]*Money, 0, len(rs))
 	for _, r := range rs {
 		party := &Money{
 			amount:   mutate.calc.allocate(m.amount, r, sum),
