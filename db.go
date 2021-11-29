a3eeaf6989fbb941e72e404e6cd25717fdb21336
David Alpert <david@spinthemoose.com>
2021-11-28 23:00:01 -0600

    GH-44 implement driver.Value and sql.Scanner for money.Money
    Money's Value() function enables compatible
    sql drivers to serialize a money.Money instance
    to a single comma-delimited string value of
    "amount,currency_code"
    
    Money's Scan() function assumes that it receives
    a single column where the src value is a comma-
    delimited string in the format
    "amount,currency_code"
    
    While the storage format is up to the client when
    the amount and currency are stored separately
    a compatible scanner value can be created in
    SQLite like this:
    
        SELECT amount || "," || currency as 'amount'
    
    It is left to the client to decide to use Money's
    Valuer implementation with a db annotation on
    a property of type Money or else to store the
    Amount and Currency values as two separate
    columns and return them as a single joined
    string field.

--- db.go
+++ db.go
@@ -3,7 +3,49 @@ package money
 import (
 	"database/sql/driver"
 	"fmt"
+	"strconv"
+	"strings"
 )
+
+// Value implements driver.Valuer to serialise a Money instance into a comma-separated string as "amount,currency_code"
+func (m *Money) Value() (driver.Value, error) {
+	return fmt.Sprintf("%d,%s", m.amount, m.Currency().Code), nil
+}
+
+// Scan implements sql.Scanner to deserialize a Money instance from an "amount,currency_code" comma-separated pair
+func (m *Money) Scan(src interface{}) error {
+	var amount Amount
+	currency := &Currency{}
+
+	// let's support string and int64
+	switch src.(type) {
+	case string:
+		parts := strings.Split(src.(string), ",")
+		if len(parts) != 2 {
+			return fmt.Errorf("%#v is not valid to scan into Money; update your query to return a comma-separated pair of \"amount,currency_code\"", src.(string))
+		}
+
+		if a, err := strconv.ParseInt(parts[0], 10, 64); err == nil {
+			amount = a
+		} else {
+			return fmt.Errorf("scanning '%#v' into an Amount: %v", parts[0], err)
+		}
+
+		if err := currency.Scan(parts[1]); err != nil {
+			return fmt.Errorf("scanning '%#v' into an Currency: %v", parts[1], err)
+		}
+	default:
+		return fmt.Errorf("don't know how to scan %T into Money; update your query to return a comma-separated pair of \"amount,currency_code\"", src)
+	}
+
+	// allocate new Money with the scanned amount and currency
+	*m = Money{
+		amount:   amount,
+		currency: currency,
+	}
+
+	return nil
+}
 
 // Value implements driver.Valuer to serialize a Currency code into a string for saving to a database
 func (c Currency) Value() (driver.Value, error) {
--- db_test.go
+++ db_test.go
@@ -2,9 +2,75 @@ package money
 
 import (
 	"database/sql/driver"
+	"fmt"
 	"reflect"
 	"testing"
 )
+
+func TestMoney_Scan(t *testing.T) {
+	tests := []struct {
+		src     interface{}
+		want    *Money
+		wantErr bool
+	}{
+		{
+			src: "10,CAD",
+			want: New(10, CAD),
+		},
+		{
+			src: "20,USD",
+			want: New(20, USD),
+		},
+		{
+			src: "30000,IDR",
+			want: New(30000, IDR),
+		},
+		{
+			src: "10",
+			wantErr: true,
+		},
+		{
+			src: "USD",
+			wantErr: true,
+		},
+		{
+			src: "USD, 10",
+			wantErr: true,
+		},
+		{
+			src: "",
+			wantErr: true,
+		},
+		{
+			src: "a,b,c",
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(fmt.Sprintf("%#v", tt.src), func(t *testing.T) {
+			got := &Money{}
+			if err := got.Scan(tt.src); (err != nil) != tt.wantErr {
+				t.Errorf("Scan() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if tt.wantErr {
+				return
+			}
+			if got == nil {
+				t.Errorf("money.Scan() result was <nil>")
+				return
+			}
+			eq, err := tt.want.Equals(got)
+			if err != nil {
+				t.Errorf(err.Error())
+			}
+			if !eq {
+				t.Errorf("Value() got = %s %s, want %s %s", got.Display(), got.Currency().Code, tt.want.Display(), tt.want.Currency().Code)
+			}
+		})
+	}
+}
+
 
 func TestCurrency_Value(t *testing.T) {
 	for code, cc := range currencies {
